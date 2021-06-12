d7025d75c3e06a21802738f24e6f59771862e18e
Sandeep Kasera <sandeepkumarkasera1@gmail.com>
2021-06-12 19:21:42 +0530

    third try

--- mtransfer/settings.py
+++ mtransfer/settings.py
@@ -133,7 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
